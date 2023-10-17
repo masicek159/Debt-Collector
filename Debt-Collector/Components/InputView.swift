2e0a00e9221a7b29a85d6bf3e434c3a438941f5b
Martin Sir <martin.sir16@gmail.com>
2023-10-17 16:48:27 -0400

    Create: InputView component

---
+++ Debt-Collector/Components/InputView.swift
@@ -0,0 +1,18 @@
+//
+//  InputView.swift
+//  Debt-Collector
+//
+//  Created by Martin Sir on 10/17/23.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InputView()
+}
