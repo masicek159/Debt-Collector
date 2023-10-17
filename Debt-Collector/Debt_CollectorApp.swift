5b3067b47cab4c8f71929fd4c59663645f5b528a
Martin Sir <martin.sir16@gmail.com>
2023-10-17 16:17:43 -0400

    Init commit

---
+++ Debt-Collector/Debt_CollectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Debt_CollectorApp.swift
+//  Debt-Collector
+//
+//  Created by Martin Sir on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Debt_CollectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
