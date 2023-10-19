53682e8a5dcd526ae12d3e9800082077f0e5863b
Marketa Minarova <marktminarov@gmail.com>
2023-10-19 15:09:15 -0400

    Add selected tab feature

--- Debt-Collector/Views/LandingView.swift
+++ Debt-Collector/Views/LandingView.swift
@@ -8,34 +8,45 @@
 import SwiftUI
 
 struct LandingView: View {
+    
+    @State var selectedTab: Int = 0
     var body: some View {
-        TabView {
+        TabView(selection: $selectedTab) {
             
             FriendsView()
                 .tabItem{
                     Image(systemName: "person.fill")
                     Text("Friends")
                 }
+                .tag(0)
+            
             ChartsView()
                 .tabItem{
                     Image(systemName: "chart.bar")
                     Text("Charts")
                 }
+                .tag(1)
+            
             AddExpenseView()
                 .tabItem{
                     Image(systemName: "plus.circle.fill")
                     Text("Add expense")
                 }
+                .tag(2)
+            
             GroupsView()
                 .tabItem{
                     Image(systemName: "person.2.fill")
                     Text("Groups")
                 }
+                .tag(3)
+            
             SettingView()
                 .tabItem{
                     Image(systemName: "circle.grid.2x2.fill")
                     Text("Settings")
                 }
+                .tag(4)
             
         }.accentColor(.purple)
         
