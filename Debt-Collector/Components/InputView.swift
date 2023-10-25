f8d5aae7f8f228a6da3fea777ed5846c145287ad
Martin Sir <martin.sir16@gmail.com>
2023-10-24 20:59:18 -0700

    Update: Input View

--- Debt-Collector/Components/InputView.swift
+++ Debt-Collector/Components/InputView.swift
@@ -9,25 +9,42 @@ import SwiftUI
 
 struct InputView: View {
     @Binding var text: String
-    let title: String
+    @FocusState var isFocused: Bool
+    @EnvironmentObject var swiftUIShared: SwiftUIShared
     let placeholder: String
     var isSecuredField = false
+    var invalidField = false
     
     var body: some View {
         VStack {
-            if title != "" {
-                Text(title)
-                    .foregroundColor(Color(.darkGray))
-                    .fontWeight(.semibold)
-                    .font(.footnote)
-            }
-            
-            if isSecuredField {
-                SecureField(placeholder, text: $text)
-                    .font(.system(size: 14))
-            } else {
-                TextField(placeholder, text: $text)
-                    .font(.system(size: 14))
+            VStack {
+                if isSecuredField {
+                    SecureInputView(placeholder, text: $text)
+                        .font(.system(size: 16))
+                        .focused($isFocused)
+                        .frame(height: 18)
+                        .simultaneousGesture(
+                            TapGesture()
+                                .onEnded { _ in
+                                    // prevent hiding keyboard
+                                }
+                        )
+                } else {
+                    TextField(placeholder, text: $text)
+                        .font(.system(size: 16))
+                        .focused($isFocused)
+                        .simultaneousGesture(
+                            TapGesture()
+                                .onEnded { _ in
+                                    // prevent hiding keyboard
+                                }
+                        )
+                        .frame(height: 18)
+                }
+                
+                Rectangle()
+                    .frame(height: 1)
+                    .foregroundColor(invalidField ? Color.red : isFocused ? Color.blue : Color.gray)
             }
         }
     }
@@ -35,6 +52,6 @@ struct InputView: View {
 
 struct InputView_Preview: PreviewProvider {
     static var previews: some View {
-        InputView(text: .constant(""), title: "", placeholder: "")
+        InputView(text: .constant(""), placeholder: "")
     }
 }
