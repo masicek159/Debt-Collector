1f5eea734400064695db6543a4601185a4ec6c9d
Martin Sir <martin.sir16@gmail.com>
2023-10-24 20:56:30 -0700

    Add: function to hide keyboard

--- Debt-Collector/Shared/SwiftUIShared.swift
+++ Debt-Collector/Shared/SwiftUIShared.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+import SwiftUI
+
+class SwiftUIShared: ObservableObject {
+    func hideKeyboard() {
+        // Resign the first responder status (dismiss the keyboard)
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
