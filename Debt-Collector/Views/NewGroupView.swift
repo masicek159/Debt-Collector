bfb3946132cd9fc3a6a8f07a68ae7a039d8be8c6
Martin Sir <martin.sir16@gmail.com>
2023-11-05 19:36:28 -0800

    Update: New group page

---
+++ Debt-Collector/Views/NewGroupView.swift
@@ -0,0 +1,45 @@
+//
+//  NewGroupView.swift
+//  Debt-Collector
+//
+//  Created by Martin Sir on 05.11.2023.
+//
+
+import SwiftUI
+
+struct NewGroupView: View {
+    @ObservedObject var viewModel: GroupsViewModel
+    @Binding var showPopup: Bool
+    
+    @State private var groupName = ""
+        @State private var groupCurrency = ""
+        @State private var groupImage = ""
+    
+    var body: some View {
+        Form {
+              Section(header: Text("Group Information")) {
+                  TextField("Name", text: $groupName)
+                  TextField("Currency", text: $groupCurrency)
+              }
+              
+              Section(header: Text("Group Image")) {
+                  TextField("Image URL", text: $groupImage)
+              }
+              
+              Button(action: {
+                  Task {
+                      try await viewModel.addGroup(name: groupName, currency: groupCurrency, image: groupImage)
+                      showPopup = false
+                      viewModel.getGroups()
+                  }
+              }) {
+                  Text("Create Group")
+              }
+          }
+          .navigationBarTitle("Create Group")
+      }
+}
+
+#Preview {
+    NewGroupView(viewModel: GroupsViewModel(), showPopup: .constant(false))
+}
