379b81018f26946055b564a3f0b8ee646d2f4253
Martin Sir <martin.sir16@gmail.com>
2023-11-06 13:53:22 -0800

    Update: Image and currency input

---
+++ Debt-Collector/Shared/CurrenciesHelper.swift
@@ -0,0 +1,8 @@
+//
+//  CurrenciesHelper.swift
+//  Debt-Collector
+//
+//  Created by Martin Sir on 06.11.2023.
+//
+
+import Foundation
--- Debt-Collector/Views/NewGroupView.swift
+++ Debt-Collector/Views/NewGroupView.swift
@@ -6,38 +6,79 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct NewGroupView: View {
     @ObservedObject var viewModel: GroupsViewModel
+    @EnvironmentObject var currenciesHelper: CurrenciesHelper
     @Binding var showPopup: Bool
     
     @State private var groupName = ""
-        @State private var groupCurrency = ""
-        @State private var groupImage = ""
+    @State private var groupCurrency = ""
+    @State private var groupImage = ""
+    
+    @State private var selectedItem: PhotosPickerItem? = nil
+    @State private var selectedImageData: Data? = nil
     
     var body: some View {
         Form {
-              Section(header: Text("Group Information")) {
-                  TextField("Name", text: $groupName)
-                  TextField("Currency", text: $groupCurrency)
-              }
-              
-              Section(header: Text("Group Image")) {
-                  TextField("Image URL", text: $groupImage)
-              }
-              
-              Button(action: {
-                  Task {
-                      try await viewModel.addGroup(name: groupName, currency: groupCurrency, image: groupImage)
-                      showPopup = false
-                      viewModel.getGroups()
-                  }
-              }) {
-                  Text("Create Group")
-              }
-          }
-          .navigationBarTitle("Create Group")
-      }
+            Section(header: Text("Group Information")) {
+                TextField("Name", text: $groupName)
+                
+                Picker("Select Currency", selection: $groupCurrency) {
+                    ForEach(currenciesHelper.currencies, id: \.self) { currency in
+                        Text(currency)
+                    }
+                }
+                .pickerStyle(.menu)
+            }
+            
+            Section(header: Text("Group Image")) {
+                PhotosPicker(
+                           selection: $selectedItem,
+                           matching: .images,
+                           photoLibrary: .shared()) {
+                               Text("Select a photo")
+                           }
+                           .onChange(of: selectedItem) { newItem in
+                               Task {
+                                   // Retrieve selected asset in the form of Data
+                                   if let data = try? await newItem?.loadTransferable(type: Data.self) {
+                                       selectedImageData = data
+//                                       TODO: save image to firestore - how to do it?
+//                                       groupImage = data.base64EncodedString()
+                                   }
+                               }
+                           }
+                       
+                       if let selectedImageData,
+                          let uiImage = UIImage(data: selectedImageData) {
+                           HStack {
+                               Spacer()
+                               Image(uiImage: uiImage)
+                                   .resizable()
+                                   .scaledToFit()
+                                   .frame(width: 250, height: 250)
+                               Spacer()
+                           }
+                       }
+            }
+            
+            Button(action: {
+                Task {
+                    try await viewModel.addGroup(name: groupName, currency: groupCurrency, image: groupImage)
+                    showPopup = false
+                    viewModel.getGroups()
+                }
+            }) {
+                Text("Create Group")
+            }
+        }
+        .navigationBarTitle("Create Group")
+        .task {
+            groupCurrency = currenciesHelper.currencies.first ?? ""
+        }
+    }
 }
 
 #Preview {
