831d0a2e37d0f0c9896fa260bb531e2c27e24953
Marketa Minarova <marktminarov@gmail.com>
2023-11-05 23:33:03 -0500

    rewrite the previews

--- Debt-Collector/Views/NewGroupView.swift
+++ Debt-Collector/Views/NewGroupView.swift
@@ -40,6 +40,8 @@ struct NewGroupView: View {
       }
 }
 
-#Preview {
+struct NewGroupView_Previews: PreviewProvider {
+    static var previews: some View {
     NewGroupView(viewModel: GroupsViewModel(), showPopup: .constant(false))
+    }
 }
