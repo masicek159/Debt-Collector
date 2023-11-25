5a8f0b616ba49a6824aa260aefdefb18e520f37c
Marketa Minarova <marktminarov@gmail.com>
2023-11-25 16:25:52 -0500

    add class GroupMemberViewModel to add members into group

--- Debt-Collector/Views/GroupDetail.swift
+++ Debt-Collector/Views/GroupDetail.swift
@@ -6,46 +6,82 @@
 //
 
 import SwiftUI
+import FirebaseAuth
+
+@MainActor
+final class GroupMemberViewModel: ObservableObject {
+    @Published private(set) var friends: [User] = []
+    
+    func addGroupMember(groupId: String, userId: String, balance: Double = 0) async throws {
+        try await GroupManager.shared.addUserGroup(groupId: groupId, userId: userId, balance: balance)
+    }
+    
+    func getFriends () {
+        Task {
+            guard let userId = Auth.auth().currentUser?.uid else { return }
+            let userFriends = try await UserManager.shared.getAllUserFriends(userId: userId)
+            
+            var localArray: [User] = []
+            
+            for userFriend in userFriends {
+                if let friend = try? await UserManager.shared.getUser(userId: userFriend.friendId) {
+                    localArray.append(friend)
+                }
+            }
+            self.friends = localArray
+        }
+    }
+    
+}
 
 struct GroupDetail: View {
     var group: GroupModel
-    
+    @ObservedObject var viewModel = GroupMemberViewModel()
+
     var body: some View {
-        List {
-            Section {
-                HStack {
-                    if let imageData = group.image, let uiImage = UIImage(data: imageData) {
-                        Image(uiImage: uiImage)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 72, height: 72)
-                    } else {
-                        // TODO: tmp obrazek postavy
-                        Image(systemName: "person.3")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 72, height: 72)
+        NavigationView {
+            List {
+                Section {
+                    HStack {
+                        if let imageData = group.image, let uiImage = UIImage(data: imageData) {
+                            Image(uiImage: uiImage)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 72, height: 72)
+                        } else {
+                            // TODO: tmp obrazek postavy
+                            Image(systemName: "person.3")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 72, height: 72)
+                        }
+                        
+                        Spacer()
+                        
+                        VStack {
+                            Text(group.name)
+                            
+                            Text(group.currency)
+                                .font(.footnote)
+                                .foregroundColor(.gray)
+                        }
                     }
                     
-                    Spacer()
-                    
-                    VStack {
-                        Text(group.name)
-                        
-                        Text(group.currency)
-                            .font(.footnote)
-                            .foregroundColor(.gray)
+                }
+                
+                Section("Members") {
+                    NavigationLink(destination: NewGroupMemberView(viewModel: viewModel)) {
+                        Text("Add Member")
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(Color.blue)
+                            .cornerRadius(8)
                     }
                 }
                 
-            }
-            
-            Section("Members") {
-                
-            }
-            
-            Section("Expenses") {
-                
+                Section("Expenses") {
+                    
+                }
             }
         }
     }
