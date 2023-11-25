4a7149df2271cc3ea9abaddc7f3acf0fbd915e72
Martin Sir <martin.sir16@gmail.com>
2023-11-25 14:23:42 -0800

    Fix: merge conflicts

--- Debt-Collector/Views/GroupDetail.swift
+++ Debt-Collector/Views/GroupDetail.swift
@@ -35,76 +35,67 @@ final class GroupMemberViewModel: ObservableObject {
 }
 
 struct GroupDetail: View {
-    @ObservedObject var viewModel = GroupsViewModel()
+    @ObservedObject var groupsViewModel = GroupsViewModel()
+    @ObservedObject var groupMemberViewModel = GroupMemberViewModel()
     
     var group: GroupModel
-    @ObservedObject var viewModel = GroupMemberViewModel()
     @State var expenses: [ExpenseModel] = []
     
+    var body: some View {
         NavigationView {
-    List {
-            Section {
-                HStack {
-                    if let imageData = group.image, let uiImage = UIImage(data: imageData) {
-                        Image(uiImage: uiImage)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 72, height: 72)
-                    } else {
-                        // TODO: tmp obrazek postavy
-                        Image(systemName: "person.3")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 72, height: 72)
+            List {
+                Section {
+                    HStack {
+                        if let imageData = group.image, let uiImage = UIImage(data: imageData) {
+                            Image(uiImage: uiImage)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 72, height: 72)
+                        } else {
+                            // TODO: tmp obrazek postavy
+                            Image(systemName: "person.3")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 72, height: 72)
+                        }
+                        
+                        Spacer()
+                        
+                        VStack {
+                            Text(group.name)
+                            
+                            Text(group.currency)
+                                .font(.footnote)
+                                .foregroundColor(.gray)
+                        }
                     }
                     
-                    Spacer()
-                    
-                    VStack {
-                        Text(group.name)
-                        
-                        Text(group.currency)
-                            .font(.footnote)
-                            .foregroundColor(.gray)
+                }
+                
+                Section("Members") {
+                    NavigationLink(destination: NewGroupMemberView(viewModel: groupMemberViewModel)) {
+                        Text("Add Member")
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(Color.blue)
+                            .cornerRadius(8)
                     }
                 }
                 
-            }
-            
-            Section("Members") {
-                NavigationLink(destination: NewGroupMemberView(viewModel: viewModel)) {
-                    Text("Add Member")
-                        .padding()
-                        .foregroundColor(.white)
-                        .background(Color.blue)
-                        .cornerRadius(8)
-                }
-            }
-            
-            Section("Expenses") {
-                ForEach(expenses, id: \.id) { expense in
-                    HStack{
-                        Text(expense.name)
-                        
-                        Text("\(expense.amount)")
-                    }
-                }
-            }
-        }
-    }
-        .onAppear {
-            Task {
-                expenses = try await viewModel.getExpenses(groupId: group.id)
+                Section("Expenses") {
+                    ForEach(expenses, id: \.id) { expense in
+                        HStack{
+                            Text(expense.name)
                             
                             Text("\(expense.amount)")
                         }
                     }
                 }
             }
-            .onAppear {
-                Task {
-                    expenses = try await viewModel.getExpenses(groupId: group.id)
-                }
+        }
+        .onAppear {
+            Task {
+                expenses = try await groupsViewModel.getExpenses(groupId: group.id)
             }
         }
     }
