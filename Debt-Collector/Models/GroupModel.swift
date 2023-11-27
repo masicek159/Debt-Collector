c41b4793ed2fba642864029c361ecefdcf3c064a
Marketa Minarova <marktminarov@gmail.com>
2023-11-27 16:19:42 -0500

    edit GroupModel, remove unnecessary functions

--- Debt-Collector/Models/GroupModel.swift
+++ Debt-Collector/Models/GroupModel.swift
@@ -14,34 +14,22 @@ class GroupModel: Codable, Identifiable, Hashable {
         lhs.id == rhs.id
     }
     public func hash(into hasher: inout Hasher) {
-           return hasher.combine(id)
-       }
+        return hasher.combine(id)
+    }
     
     var id: String = ""
     var members: [User] = []
     var name: String
     var currency: String // TODO: add currency model or currency enum
     var image: Data?
-
+    
     init(id: String, name: String, currency: String, image: Data?, owner: User) {
         self.id = id
         self.image = image
         self.name = name
         self.currency = currency
-        self.members = [owner]
     }
     
-    func addMember(member: User){
-        members.append(member)
-    }
-    func removeMember(member: User) {
-        if let index = members.firstIndex(where: { $0.id == member.id }) {
-            members.remove(at: index)
-        }
-    }
-    func getAllMembers() -> [User] {
-        return members
-    }
     func getGroupID() -> String {
         return id
     }
@@ -63,13 +51,4 @@ class GroupModel: Codable, Identifiable, Hashable {
     func setGroupImage(newImage: Data?) {
         image = newImage
     }
-    func processPayment(value: Int, payer: User){
-        //Ensuring payer is in group
-        guard members.contains(where: {$0.id == payer.id}) else {
-            return
-        }
-        
-        let paymentPerMember = value / members.count
-    }
 }
-
