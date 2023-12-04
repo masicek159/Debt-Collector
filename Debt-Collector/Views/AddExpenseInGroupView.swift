31379f747dc0df81b6bdbd687fd9b17dd05ae510
Matt Allen Rieger <marieger@HLIBMedia-MA2D7.uvm.edu>
2023-12-04 17:51:52 -0500

    fixed group expense view

--- Debt-Collector/Views/AddExpenseInGroupView.swift
+++ Debt-Collector/Views/AddExpenseInGroupView.swift
@@ -1,5 +1,5 @@
 //
-//  AddExpenseView.swift
+//  AddExpenseInGroupView.swift
 //  Debt-Collector
 //
 //  Created by user248815 on 10/19/23.
@@ -11,18 +11,19 @@ struct AddExpenseInGroupView: View {
     @ObservedObject var groupViewModel = GroupViewModel()
     @ObservedObject var expenseViewModel = ExpenseViewModel()
     @EnvironmentObject var categoryViewModel: CategoryViewModel
+    
     var group: GroupModel
     @Binding var showAddExpensePopUp: Bool
     
-    @State var name: String = ""
-    @State var amount: Double = 0.0
-    @State var uploadingExpense = false
-    @State var category: Category? = nil
-    @State var expenseCurrency: String = "USD"
-    @State var paidBy: User? = AuthViewModel.shared.currentUser
-    @State var expenseAdded = false
-    @State var dateCreated: Date = Date()
-    @State var selectedParticipants: [Participant] = []
+    @State private var name: String = ""
+    @State private var amount: Double = 0.0
+    @State private var uploadingExpense = false
+    @State private var category: Category? = nil
+    @State private var expenseCurrency: String = "USD"
+    @State private var paidBy: User? = AuthViewModel.shared.currentUser
+    @State private var expenseAdded = false
+    @State private var dateCreated: Date = Date()
+    @State private var selectedParticipants: [Participant] = []
     
     @Binding var participants: [Participant]
     @Binding var sharesNotSpecified: Bool
@@ -34,92 +35,83 @@ struct AddExpenseInGroupView: View {
     }()
     
     var body: some View {
-        Section (header: HStack {
-            Text("Add an expense")
-                .font(.title)
-            
-            Spacer()
-            
-            Button(action: {
-                showAddExpensePopUp = false
-            }) {
-                Text("Cancel")
-            }
-        }) {
+        NavigationView {
             Form {
-                Text(group.name)
-                
-                TextField("Name", text: $name)
-                
-                Picker("Category", selection: $category) {
-                    ForEach(categoryViewModel.categories , id: \.id) { category in
-                        Text(category.name)
-                            .tag(category as Category?)
-                    }
-                }
-                .pickerStyle(.menu)
-                
-                TextField("Amount", value: $amount, formatter: decimalFormatter)
-                
-                Picker("Select Currency", selection: $expenseCurrency) {
-                    ForEach(CurrenciesHelper.shared.currencies, id: \.self) { currency in
-                        Text(currency).tag(currency)
-                    }
-                }
-                .pickerStyle(.menu)
-                
-                Picker("Select Who Paid", selection: $paidBy) {
-                    ForEach(group.membersAsUsers, id: \.id) { user in
-                        Text(user.fullName)
-                            .tag(user as User?)
-                    }
-                }
-                .pickerStyle(.menu)
-                
-                MultiSelector(
-                    totalAmount: $amount,
-                    participants: $participants,
-                    selectedParticipants: $selectedParticipants,
-                    sharesNotSpecified: $sharesNotSpecified
-                )
-                
-                DatePicker(
-                    "Date",
-                    selection: $dateCreated,
-                    displayedComponents: [.date]
-                )
-                
-                Button(action: {
-                    Task {
-                        if let paidBy = paidBy {
-                            uploadingExpense = true
-                            
-                            var total: Double = 0
-                            for participant in selectedParticipants {
-                                total += participant.amountToPay
-                            }
-                            
-                            if total != amount {
-                                // change the totalAmounts
-                                amount = total
-                            }
-                            try await expenseViewModel.addExpense(name: name, amount: amount, category: category, currency: expenseCurrency, groupId: group.id, paidBy: paidBy, participants: Array(participants), dateCreated: dateCreated)
-                            uploadingExpense = false
-                            showAddExpensePopUp = false
+                Section() {
+                    TextField("Name", text: $name)
+                    Picker("Category", selection: $category) {
+                        ForEach(categoryViewModel.categories, id: \.id) { category in
+                            Text(category.name).tag(category as Category?)
                         }
                     }
-                }) {
-                    Text("Add Expense")
+                    .pickerStyle(.menu)
+                    TextField("Amount", value: $amount, formatter: decimalFormatter)
+                    Picker("Select Currency", selection: $expenseCurrency) {
+                        ForEach(CurrenciesHelper.shared.currencies, id: \.self) { currency in
+                            Text(currency).tag(currency)
+                        }
+                    }
+                    .pickerStyle(.menu)
+                    Picker("Select Who Paid", selection: $paidBy) {
+                        ForEach(group.membersAsUsers, id: \.id) { user in
+                            Text(user.fullName).tag(user as User?)
+                        }
+                    }
+                    .pickerStyle(.menu)
+                    MultiSelector(
+                        totalAmount: $amount,
+                        participants: $participants,
+                        selectedParticipants: $selectedParticipants,
+                        sharesNotSpecified: $sharesNotSpecified
+                    )
+                    DatePicker("Date", selection: $dateCreated, displayedComponents: [.date])
+                }
+                
+                Section {
+                    Button(action: {
+                        Task {
+                            if let paidBy = paidBy {
+                                uploadingExpense = true
+                                
+                                var total: Double = 0
+                                for participant in selectedParticipants {
+                                    total += participant.amountToPay
+                                }
+                                
+                                if total != amount {
+                                    amount = total
+                                }
+                                
+                                try await expenseViewModel.addExpense(
+                                    name: name,
+                                    amount: amount,
+                                    category: category,
+                                    currency: expenseCurrency,
+                                    groupId: group.id,
+                                    paidBy: paidBy,
+                                    participants: Array(participants),
+                                    dateCreated: dateCreated
+                                )
+                                
+                                uploadingExpense = false
+                                showAddExpensePopUp = false
+                            }
+                        }
+                    }) {
+                        Text("Add Expense")
+                    }
+                    .disabled(uploadingExpense)
                 }
             }
-            .disabled(uploadingExpense)
-        }
-        .padding()
-        .task {
-            category = categoryViewModel.categories.filter{$0.name == "General"}.first
-            paidBy = AuthViewModel.shared.currentUser
-            await groupViewModel.getGroups()
-            expenseCurrency = group.currency
+            .padding()
+            .navigationBarTitle("Add Expense", displayMode: .inline)
+            .navigationBarItems(trailing: Button("Cancel") {
+                showAddExpensePopUp = false
+            })
+            .task {
+                category = categoryViewModel.categories.first
+                paidBy = AuthViewModel.shared.currentUser
+            }
         }
     }
 }
