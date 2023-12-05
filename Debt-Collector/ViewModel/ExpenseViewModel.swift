4d2602624c8fde934d814d4d4d3f8fa38a9c1a95
Matthew Rieger <mrfergenson@gmail.com>
2023-12-04 19:42:33 -0500

    updated friends page balances

--- Debt-Collector/ViewModel/ExpenseViewModel.swift
+++ Debt-Collector/ViewModel/ExpenseViewModel.swift
@@ -19,7 +19,6 @@ final class ExpenseViewModel: ObservableObject {
         for participant in participants {
             try await GroupManager.shared.updateBalance(groupId: groupId, memberId: participant.userId, addAmount: false, amount: participant.amountToPay)
         }
-        
         // add balance to the payer
         try await GroupManager.shared.updateBalance(groupId: groupId, memberId: paidBy.id, addAmount: true, amount: amount)
     }
